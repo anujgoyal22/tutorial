e5e09de1430aa91debc88f2ee945de1a6bcf087f
Anuj~ <anujgoyal21@yahoo.co.in>
2023-08-23 03:09:42 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        ?<meta name="viewport"! content="width=device-width, initial-scale=1.0">
+        <title>Registration Form</title>
+</head>
+<h1>Blood Donation Camp</h1>
+<body bgcolor="FB8917">
+    
+</body>
